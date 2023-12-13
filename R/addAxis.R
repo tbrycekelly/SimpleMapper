06472a1e50d2e9c2360ef722eeebbe89510ae99e
tbrycekelly <tbrycekelly@gmail.com>
2023-12-13 12:20:10 -0900

    Fix axis issues

--- R/addAxis.R
+++ R/addAxis.R
@@ -20,14 +20,16 @@ addAxis = function(basemap,
   par(las=1)
   usr = par()$usr
   N = 5000 * 4e3 / diff(usr[1:2])
+  dx = diff(usr[1:2]) / N * 1e3
+  dy = diff(usr[3:4]) / N * 1e3
   
   ## Add lon axes to sides 1 & 2
   for (ll in lons) {
-    tmp = basemap$projection(lon = rep(ll, N) - basemap$lon,
-                             lat = seq(-90, 90, length.out = N),
-                             lon0 = 0,
+    tmp = basemap$projection(lon = rep(ll, N),
+                             lat = seq(min(lats), max(lats), length.out = N),
+                             lon0 = basemap$lon,
                              lat0 = basemap$lat)
-    k = !is.na(tmp$x) & !is.na(tmp$y) & tmp$x >= usr[1] & tmp$x <= usr[2] & tmp$x >= usr[3] & tmp$y <= usr[4]
+    k = !is.na(tmp$x) & !is.na(tmp$y) & tmp$x >= usr[1] - dx & tmp$x <= usr[2] + dx & tmp$y >= usr[3] - dy & tmp$y <= usr[4] + dy
     
     if (sum(k) > 0) {
       tmp$x[!k] = NA
@@ -46,10 +48,10 @@ addAxis = function(basemap,
             s = 'W'; ll = -ll 
           }
           
-          if (diff(range(sign(tmp$y[split[[i]]] - usr[1]))) > 0 & 1 %in% label.sides) { # TODO add angle
+          if (diff(range(sign(tmp$y[split[[i]]] - usr[3]))) > 0 & 1 %in% label.sides) { # TODO add angle
             axis(1, at = approx(tmp$y[split[[i]]], tmp$x[split[[i]]], xout = usr[3], rule = 2)$y, labels = paste(ll, s))
           }
-          if (diff(range(sign(tmp$y[split[[i]]] - usr[2]))) > 0 & 3 %in% label.sides) {
+          if (diff(range(sign(tmp$y[split[[i]]] - usr[4]))) > 0 & 3 %in% label.sides) {
             axis(3, at = approx(tmp$y[split[[i]]], tmp$x[split[[i]]], xout = usr[4], rule = 2)$y, labels = paste(ll, s))
           }
         }
@@ -58,12 +60,12 @@ addAxis = function(basemap,
   }
   
   for (ll in lats) {
-    tmp = basemap$projection(lon = seq(-180, 180, length.out = N) - basemap$lon,
+    tmp = basemap$projection(lon = seq(min(lons), max(lons), length.out = N),
                              lat = rep(ll, N),
-                             lon0 = 0,
+                             lon0 = basemap$lon,
                              lat0 = basemap$lat)
     
-    k = !is.na(tmp$x) & !is.na(tmp$y) & tmp$x >= usr[1] & tmp$x <= usr[2] & tmp$y >= usr[3] & tmp$y <= usr[4]
+    k = !is.na(tmp$x) & !is.na(tmp$y) & tmp$x >= usr[1] - dx & tmp$x <= usr[2] + dx & tmp$y >= usr[3] - dy & tmp$y <= usr[4] + dy
     
     if (sum(k) > 0) {
       tmp$x[!k] = NA
@@ -83,10 +85,10 @@ addAxis = function(basemap,
             s = 'S'; ll = -ll 
           }
           
-          if (diff(range(sign(tmp$x[split[[i]]] + usr[3]))) > 0 & 2 %in% label.sides) {
+          if (diff(range(sign(tmp$x[split[[i]]] - usr[1]))) > 0 & 2 %in% label.sides) {
             axis(2, at = approx(tmp$x[split[[i]]], tmp$y[split[[i]]], xout = usr[2], rule = 2)$y, labels = paste(ll, s))
           }
-          if (diff(range(sign(tmp$x[split[[i]]] - usr[4]))) > 0 & 4 %in% label.sides) {
+          if (diff(range(sign(tmp$x[split[[i]]] - usr[2]))) > 0 & 4 %in% label.sides) {
             axis(4, at = approx(tmp$x[split[[i]]], tmp$y[split[[i]]], xout = usr[4], rule = 2)$y, labels = paste(ll, s))
           }
         }
